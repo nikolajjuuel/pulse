654fd548f6608b749f2c8f16e3a2740691df76ff
Nikolaj Johansen <nikolaj.juuel@gmail.com>
2024-11-28 21:01:01 -0800

    database set up

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,10 +1,17 @@
 import { Request, Response } from "express";
+import { db } from "../db/db";
+import { usersTable } from "../db/schema";
 
-export const getUsers = (req: Request, res: Response) => {
-  res.json({ message: "List of users" });
+export const getUsers = async (req: Request, res: Response) => {
+  const users = await db.select().from(usersTable);
+  try {
+    res.json({ message: "List of users", body: users });
+  } catch (error) {
+    res.json({ message: "Error getting users", body: error });
+  }
 };
 
-export const createUser = (req: Request, res: Response) => {
-  const { name } = req.body;
-  res.json({ message: `User ${name} created successfully!` });
+export const createUser = async (req: Request, res: Response) => {
+  const users = await db.insert(usersTable).values(req.body).returning();
+  res.status(201).json({ message: "User created successfully", body: users });
 };
